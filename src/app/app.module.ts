58dff4919b6d29fb6fd09326f391d5b23bc164dc
beneagheorghe <benea.gheorghe@mail.ru>
2020-06-10 16:40:13 +0300

    shopping-ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { ShoppingReducer } from "./store/reducers/shopping.reducer";
+
+import { AppComponent } from "./app.component";
+import { environment } from "src/environments/environment";
+import { EffectsModule } from "@ngrx/effects";
+import { ShoppingEffects } from "./store/effects/shopping.effect";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer,
+    }),
+    EffectsModule.forRoot([ShoppingEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
